df3f498aaba2446fce69b7b655200afd963f8a7d
Victor Iwegbu <iwegbuvc@gmail.com>
2026-03-07 14:57:41 +0100

    changed mail

--- controller/contactController.js
+++ controller/contactController.js
@@ -9,7 +9,7 @@ async function handleContactForm(req, res) {
   const { name, email, message } = req.body;
   try {
     await sendMail(
-      "iwegbuvc@gmail.com", // recipient
+      "alexokoriengo@gmail.com", // recipient
       `Contact Form Message from ${name}`,
       `<p><strong>Email:</strong> ${email}</p><p>${message}</p>`,
       name,
